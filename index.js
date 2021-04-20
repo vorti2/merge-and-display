5f199460af503cee862248c68052efa15262d92d
Wilhelm Vortisch <wv@v-software.de>
2021-04-20 16:39:56 +0200

    wip (read from file)

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-// const parsed = JSON.parse('[{"name":"john", "place":"usa", "phone":"12345"},{"name":"jim", "place":"canada", "phone":"54321"}]');
+#!/usr/bin/env node
+
+'use strict';
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const fs = require('fs')
+const path = require( "path" )
 
 
-
-const parsed = JSON.parse('[[25,30],[2,19],[14, 23],[4,8]]');
-
-const arr = [];
-for (var i=0; i < parsed.length; i++) {
-   var item = [parsed[i][0], parsed[i][1]];
-   arr.push(item);
+function main() {
+  yargs(hideBin(process.argv))
+    .command('test [filename]', 'execute merge with test-data', (yargs) => {
+      yargs
+        .positional('test', {
+          describe: 'read test-data from file <filename>',
+        })
+    }, (argv) => {
+      if (argv.verbose) console.info(`use as input: ${argv.filename}`)
+      // serve(argv.port)
+      const data = getFileContent(argv.filename, argv)
+      parse(data)
+    })
+    .option('verbose', {
+      alias: 'v',
+      type: 'boolean',
+      description: 'Run with verbose logging'
+    })
+    .option('help', {
+      alias: 'h',
+      type: 'boolean',
+      description: 'Help for usage'
+    })
+    .argv
 }
 
-console.log(`item = >${JSON.stringify(arr)}<`);
+
+
+function getFileContent (fileName, argv) {
+
+  try {
+    const absolutePath = path.normalize( fileName );
+    const data = fs.readFileSync(absolutePath, 'utf8')
+    if (argv.verbose)
+      console.log(`Data read from file ${fileName}: ${data}`)
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+function parse(data) {
+  // is data real JSON?
+
+  const parsed = JSON.parse(data)
+  const arr = [];
+  for (var i=0; i < parsed.length; i++) {
+    var item = [parsed[i][0], parsed[i][1]];
+    arr.push(item);
+  }
+
+  console.log(`item = >${JSON.stringify(arr)}<`)
+
+}
+
+
+
+
+
+main()
+
